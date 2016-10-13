f7ab456f928daeff0c3817415551575c96a3a282
ArtoriusXIII <arthur.janka@gmail.com>
2016-10-13 13:26:34 +0000

    adding default image

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-    has_attached_file :image, styles: { medium: "300x500>", thumb: "800x800>" }, default_url: "default-thumbnail.jpg"
+    has_attached_file :image, styles: { medium: "300x500>", thumb: "800x800>" }, default_url: "default.jpg"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
